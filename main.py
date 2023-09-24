f6dff870476df0de26a88194d0044a7b310e4813
namseokyoo <namseok.yoo@gmail.com>
2023-09-24 15:22:39 +0900

    add buy me a coffee

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-from langchain.chat_models import ChatOpenAI
+# from langchain.chat_models import ChatOpenAI
 import streamlit as st
+from langchain.llms import CTransformers
+from streamlit_extras.buy_me_a_coffee import button
 # from dotenv import load_dotenv
 # load_dotenv()
 
-# 말을 이어서하는 모드
-# from langchain.llms import OpenAI
-# llm = OpenAI()
-# result =llm.predict("오늘 기분이 약간 별로야, 날 좀 응원해줄 수 있니?")
+
+# chat_model = llama
+llm = CTransformers(
+    model="llama-2-7b-chat.ggmlv3.q2_K.bin",
+    model_type="llama"
+)
+
+# buy me a coffee
+button(username="namseokyoo", floating=True, width=221)
 
 
-# 채팅을 하는 모드
-chat_model = ChatOpenAI()
+# Chat_model = ChatOpenAI()
+# llm = ChatOpenAI()
 
 st.title('인공지능 시인 :sunglasses:')
 
@@ -18,5 +25,7 @@ content = st.text_input('시의 주제를 입력해 주세요', )
 
 if st.button('시 작성 요청하기'):
     with st.spinner('시를 쓰고 있습니다...'):
-        result = chat_model.predict(content + "에 대한 시를 써줘")
+        # result = llm.predict(content + "에 대한 시를 써줘")
+        # result = llm.predict("please write a poem about" + content + ":")
+        result = llm.predict("hello")
         st.write(result)
